a6133e37059450229bef56625947602bce7c85dc
dendenario <denisdenario@gmail.com>
2021-09-27 22:04:48 +0300

    data objects added (Author and Book)

---
+++ src/main/java/com/example/MyBookShopApp/data/Author.java
@@ -0,0 +1,22 @@
+package com.example.MyBookShopApp.data;
+
+public class Author {
+    private String firstName;
+    private String secondName;
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+}
