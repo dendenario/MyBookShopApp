24ab355711fb7667008407bc6061079a71648fea
dendenario <denisdenario@gmail.com>
2021-09-27 22:05:15 +0300

    Author and Book services added

---
+++ src/main/java/com/example/MyBookShopApp/data/AuthorService.java
@@ -0,0 +1,31 @@
+package com.example.MyBookShopApp.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AuthorService {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public AuthorService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Author> getAuthorsData()
+    {
+        List<Author> authorsList = jdbcTemplate.query("SELECT * FROM authors", (ResultSet rs, int rowNum) -> {
+            Author author = new Author();
+            author.setFirstName(rs.getString("firstname"));
+            author.setSecondName(rs.getString("secondname"));
+            return author;
+        });
+        return new ArrayList<>(authorsList);
+    }
+}
